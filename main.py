50b5460eea7f2ff4d6b88b0bf15c633629e9a7f8
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-15 22:26:16 +0300

    ceat

---
+++ main.py
@@ -0,0 +1,32 @@
+#импортируем Flask и библиотеку Request
+from flask import Flask, render_template, request
+import requests
+
+#импортируем объект класса Flask
+app = Flask(__name__)
+
+#формируем путь и методы GET и POST
+@app.route('/', methods=['GET', 'POST'])
+#создаем функцию с переменной weather, где мы будем сохранять погоду
+def index():
+   weather = None
+#формируем условия для проверки метода. Форму мы пока не создавали, но нам из неё необходимо будет взять только город.
+   if request.method == 'POST':
+#этот определенный город мы будем брать для запроса API
+       city = request.form['city']
+#прописываем переменную, куда будет сохраняться результат и функцию weather с указанием города, который берем из формы
+       weather = get_weather(city)
+   return render_template("index.html", weather=weather)
+
+#в функции прописываем город, который мы будем вводить в форме
+def get_weather(city):
+   api_key = "69b5c0453341a541b802c116ad211f8f"
+   #адрес, по которомы мы будем отправлять запрос. Не забываем указывать f строку.
+   url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+   #для получения результата нам понадобится модуль requests
+   response = requests.get(url)
+   #прописываем формат возврата результата
+   return response.json()
+
+if __name__ == '__main__':
+   app.run(debug=True)
