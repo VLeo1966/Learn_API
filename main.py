402ac3187b4e858c08ecc6109fa34e099a1c52a9
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-16 05:33:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-from flask import Flask, render_template
 import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-# Главная страница
 @app.route('/')
 def home():
     try:
-        # Запрашиваем случайную цитату с API
+        # Отправляем запрос к API
         response = requests.get('https://api.quotable.io/random')
-        response.raise_for_status()  # Проверяем наличие ошибок HTTP
+        response.raise_for_status()  # Проверяем на ошибки HTTP
         data = response.json()
+        print(data)  # Выводим результат в консоль для проверки
 
-        # Получаем цитату и автора из данных
-        quote = data['content']
-        author = data['author']
+        # Извлекаем цитату и автора
+        quote = data.get('content')
+        author = data.get('author')
     except requests.exceptions.RequestException as e:
+        # Обрабатываем ошибки запроса
         quote = "Не удалось получить цитату. Попробуйте позже."
         author = ""
 
-    # Передаем цитату и автора в шаблон
     return render_template('index3.html', quote=quote, author=author)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-#
